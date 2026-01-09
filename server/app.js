2b3d838f18718b9c5fd184724385bca2a48b95b4
Charlie-robin <charlie.robin.richardson@gmail.com>
2026-01-09 14:18:36 +0000

    Added demo code

--- server/app.js
+++ server/app.js
@@ -3,16 +3,19 @@ import { WebSocketServer } from "ws";
 const server = new WebSocketServer({ port: 5001 });
 
 const greeting = JSON.stringify({
-  content: "Hello from the server!",
+  content: "Hello World",
+  id: "greet-0",
+  senderId: "0",
 });
 
 server.on("connection", (socket) => {
-  // THIS IS NOT THE DATA SCHEMA THE CLIENT EXPECTS
   socket.send(greeting);
 
   socket.on("message", (message) => {
     const clientMessage = JSON.parse(message);
-    // THIS IS NOT THE DATA SCHEMA THE CLIENT EXPECTS
-    socket.send(JSON.stringify({ content: clientMessage.content }));
+    // DATA IS NOT 100% CORRECT RIGHT NOW
+    socket.send(
+      JSON.stringify({ content: clientMessage.content.toUpperCase() })
+    );
   });
 });
